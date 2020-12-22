b35b7fa3b62619e05621eaa0ed514eb10a895368
Rfontt <69485548+Rfontt@users.noreply.github.com>
2020-12-22 11:07:09 -0300

    Update UpdatePassword.js
    Modificando o controller de password para alterar a forma de receber o id. Antes era pelo endpoint e agora é através do result.token.id_user que nos traz essa informação de forma mais explicita e menos complicada.

--- src/Controllers/Users_Adm/UpdatePassword.js
+++ src/Controllers/Users_Adm/UpdatePassword.js
@@ -4,9 +4,8 @@ const User = require('../../Models/UsersAdm');
 
 async function RecoverPassword(req, res) {
   var { password, token } = req.body;
-  var id = req.params.id;
 
-  if ( id == undefined || password == undefined || token == undefined ) {
+  if (password == undefined || token == undefined ) {
     res.status(400).send({
       Message: "Field is undefined"
     })
@@ -14,7 +13,8 @@ async function RecoverPassword(req, res) {
 
   var result = await PasswordToken.validate(token);
   if (result.status) {
-    var newPassword = await User.updatePassword(id, password, result.token.token);
+    await User.updatePassword(result.token.id_user, password, result.token.token);
+    
     res.status(201).send({
       Message: "Password updated"
     })
